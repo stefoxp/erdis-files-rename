aa8f69de17ea377463a1e7f07f38d1c6a5f29019
Stefano P <33906742+stefoxp@users.noreply.github.com>
2021-02-25 11:26:49 +0100

    Rename all photo's file on list and log missing files (close #4 )

---
+++ foto_dipendenti.py
@@ -0,0 +1,46 @@
+from libreria import csv_file, files
+
+INPUT_PATH: str = 'dati/'
+OUTPUT_PATH: str = 'dati/output_files/'
+EXTENSIONS: list = ['.jpg']
+FILE_NAME_LEN: int = 10
+
+
+def main():
+    csv_obj = csv_file.Csv(INPUT_PATH, "anagrafica.csv", "log.txt", ";")
+    anagrafica_list = csv_obj.file_to_list()
+
+    # debug
+    print(anagrafica_list)
+
+    # riformatta il numero badge
+    anagrafica_list = formatta_file_name(anagrafica_list)
+
+    # debug
+    print('anagrafica_list:', anagrafica_list)
+
+    files_obj = files.Files(INPUT_PATH + 'foto/')
+    files_obj.rename(anagrafica_list, EXTENSIONS, OUTPUT_PATH)
+
+    files_name_calculate = files.get_files_list(OUTPUT_PATH)
+
+    # debug
+    print(files_name_calculate)
+
+
+def formatta_file_name(anagrafica_list):
+    anagrafica_badge_da_dieci_list = []
+    for anagrafica in anagrafica_list:
+        zero_str: str = ''
+        for zero in range(FILE_NAME_LEN - len(anagrafica[1])):
+            zero_str += '0'
+
+        badge = zero_str + anagrafica[1]
+        anagrafica_tup = (anagrafica[0], badge, anagrafica[2])
+
+        anagrafica_badge_da_dieci_list.append(anagrafica_tup)
+    return anagrafica_badge_da_dieci_list
+
+
+if __name__ == '__main__':
+    main()
